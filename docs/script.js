57ce80abd6c43e8fb885aebbc1da0246731097bd
RaulMdzLemus <rulo102000@gmail.com>
2025-07-24 18:55:42 -0600

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -28,7 +28,7 @@ fetch('./data/ALC_REPROJECT.geojson')
   });
 
 // Cargar predios
-fetch('./data/PREDIOS%SERVIMET%4326.geojson')
+fetch('/data/BOLSA_INMOBILIARIA.geojson')
   .then(res => res.json())
   .then(data => {
     var prediosGeo = L.geoJSON(data, {
